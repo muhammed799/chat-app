00e0424d26313233d21cca881507b03a2495ae87
Mohammed Thaha Abdul Shukoor <mthahaab@rmplc.net>
2021-06-25 22:58:19 +0530

    m

--- server/routes/message.js
+++ server/routes/message.js
@@ -2,12 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 
+
 const message_model = require("../models/message.model")
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', async function(req, res, next) {
+  try {
+    let page_no = req.body.page;
+    let limit = req.body.limit;
+
+    let ln = parseInt(limit);
+    const messages_details = await message_model.find().limit(ln).skip((page_no-1) * limit);
+    res.send(messages_details)
+  } catch (error) {
+    res.sendStatus(500).send(error.message);
+  }
 });
 
 module.exports = router;
